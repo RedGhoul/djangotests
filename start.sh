dcbca6eabe7691ac80b25afab01da34a00f78005
RedGhoul <avaneesab5@gmail.com>
2020-10-11 10:15:18 -0400

    fixed ssh ???

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 #start second servvice here, and push it to background:
-python manage.py qcluster &
+gunicorn tutorialSite.wsgi:application --workers=3 --bind 0.0.0.0:8080 &
 
-gunicorn tutorialSite.wsgi:application --workers=3 --bind 0.0.0.0:8080
+python manage.py qcluster 

f2629c6dc5963cc3657abe80bc40f604ec8960f6
RedGhoul <avaneesab5@gmail.com>
2020-10-11 01:40:19 -0400

    hell

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-
-
-
 #! /bin/bash
 
 #start second servvice here, and push it to background:
-python manage.py qcluster &
+#python manage.py qcluster &
 
-#then run the last commands:
 gunicorn tutorialSite.wsgi:application --workers=3 --bind 0.0.0.0:8080

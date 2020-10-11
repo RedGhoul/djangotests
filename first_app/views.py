f64ab4a16a25e5b9cdeae1a80b28665b74f25c99
RedGhoul <avaneesab5@gmail.com>
2020-10-11 10:44:24 -0400

    added test route

--- first_app/views.py
+++ first_app/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 def index(request):
     my_dict = {'insert_me': "Hellp i am trying to get out of the machine"}
     return render(request, 'first_app/index.html', context=my_dict)
+
+
+def test(request):
+    return HttpResponse("<h1>Test</h1>")
